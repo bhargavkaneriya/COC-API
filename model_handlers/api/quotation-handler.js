7ece0f218f6e1111910e6a480c2fb8a1abe4b685
keval-patel-xl <kevalpatelce@gmail.com>
2023-08-04 00:44:20 +0530

    latest

--- model_handlers/api/quotation-handler.js
+++ model_handlers/api/quotation-handler.js
@@ -132,7 +132,7 @@ const createQuotation = (requestParam) => {
         console.log("Current Directory Path:", currentDirectoryPath);
 
         console.log("randomStr", randomStr);
-        let pdfPath = `../public/pdf/${randomStr}.pdf`
+        let pdfPath = `${randomStr}.pdf`
 console.log("pdfPath",pdfPath);
         // pdf.create(htmlContent, pdfOptions)
         //   .toFile(pdfPath, (err, res) => {

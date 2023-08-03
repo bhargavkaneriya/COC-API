2314b3eb9d8dcb8017f8f36e819669d0a92ce735
keval-patel-xl <kevalpatelce@gmail.com>
2023-08-04 02:47:52 +0530

    latest

--- model_handlers/api/quotation-handler.js
+++ model_handlers/api/quotation-handler.js
@@ -150,35 +150,35 @@ const createQuotation = (requestParam) => {
                 reject(err);
                 return;
               }
-              console.log("pdf res", res);
-              // var AWS = require("aws-sdk");
-              // let s3 = new AWS.S3();
+              // console.log("pdf res", res);
+              var AWS = require("aws-sdk");
+              let s3 = new AWS.S3();
 
-              // const params = {
-              //   Bucket: config.aws.s3.cocBucket,
-              //   Key: `${randomStr}.pdf`,
-              //   Body: fs.readFileSync(pdfPath),
-              //   ContentType: "application/pdf",
-              //   ACL: "public-read",
-              // };
+              const params = {
+                Bucket: config.aws.s3.cocBucket,
+                Key: `${randomStr}.pdf`,
+                Body: fs.readFileSync(pdfPath),
+                ContentType: "application/pdf",
+                ACL: "public-read",
+              };
 
-              // fs.unlink(`./public/pdf/${randomStr}.pdf`, (err) => {
-              //   if (err) {
-              //     console.log("err", err);
-              //   };
-              // });
+              fs.unlink(`./public/pdf/${randomStr}.pdf`, (err) => {
+                if (err) {
+                  console.log("err", err);
+                };
+              });
 
-              // let dataUpload = s3.upload(params, async (err, data) => {
-              //   if (err) {
-              //     console.log("error", err);
-              //     reject(err); // If you're using promises, you can reject here.
-              //     return;
-              //   }
-              //   console.log("data", data);
-              //   console.log("data.Location", data.Location);
-              //   resolve(data.Location);
-              //   return;
-              // });
+              let dataUpload = s3.upload(params, async (err, data) => {
+                if (err) {
+                  console.log("error", err);
+                  reject(err); // If you're using promises, you can reject here.
+                  return;
+                }
+                console.log("data", data);
+                console.log("data.Location", data.Location);
+                resolve(data.Location);
+                return;
+              });
 
             });
 
